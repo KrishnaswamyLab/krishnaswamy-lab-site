1bf40953b751d9c6b874227b43615bd3894145b4
dsm-72 <100158155+dsm-72@users.noreply.github.com>
2023-02-15 12:26:58 -0500

    fix adapator

--- svelte.config.js
+++ svelte.config.js
@@ -11,14 +11,14 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		
-		// if true, will create a Netlify Edge Function rather
-		// than using standard Node-based functions
-		edge: false,
+		// // if true, will create a Netlify Edge Function rather
+		// // than using standard Node-based functions
+		// edge: false,
 	
-		// if true, will split your app into multiple functions
-		// instead of creating a single one for the entire app.
-		// if `edge` is true, this option cannot be used
-		split: false
+		// // if true, will split your app into multiple functions
+		// // instead of creating a single one for the entire app.
+		// // if `edge` is true, this option cannot be used
+		// split: false
 	}
 };
 
