5fc25530702c53db18502838ea5409f00b15e376
dsm-72 <100158155+dsm-72@users.noreply.github.com>
2023-02-15 12:26:36 -0500

    fix adapator

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
-// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import adapter from '@sveltejs/adapter-netlify';
+// import adapter from '@sveltejs/adapter-netlify';
  
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
