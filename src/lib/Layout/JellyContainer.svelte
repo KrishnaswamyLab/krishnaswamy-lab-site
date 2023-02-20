c8fdc1f1c5dbdf1be1a39e3068a091dd9636e117
dsm-72 <100158155+dsm-72@users.noreply.github.com>
2023-02-20 08:58:32 -0500

    jelly use

--- src/lib/Layout/JellyContainer.svelte
+++ src/lib/Layout/JellyContainer.svelte
@@ -22,8 +22,8 @@
     max-width: 68rem;
 }
 </style>
-<div class="flex justify-center">
-    <div class="mx-8 w-5/6 xl:w-[72rem]">
+<div class="flex justify-center {$$props.class}">
+    <div class="mx-8 w-5/6 md:w-[48rem] xl:w-[72rem]">
         <slot>
 
         </slot>
--- src/routes/teaching/+page.svelte
+++ src/routes/teaching/+page.svelte
@@ -7,34 +7,30 @@ import TextHero from '$lib/Layout/TextHero.svelte';
 import Hero from '$lib/Layout/Hero.svelte';
 </script>
 <Hero></Hero>
-
-
-<div class="mt-8 flex justify-center">
-    <div class="mx-8 w-5/6 md:w-[48rem]">
-        <div class="font-extralight text-7xl text-center mb-8">
-            Selected Yale University Courses
-        </div>
-        {#each data?.courses as {title, about, professors, listings}}
-        <div class="mt-8">
-            <h2 class="font-light text-4xl my-2">
-                {title}
-            </h2>
-            <div>
-                <h3 class="font-extralight text-2xl my-2">
-                    {#each professors as professor, i}
-                        {professor}{i < professors.length - 1 ? " & " : ''}
-                    {/each}
-                </h3>
-                <span class="font-semibold text-lg my-2">
-                    {#each listings as listing, i}
-                        {listing}{i < listings.length - 1 ? "/" : ''}
-                    {/each}
-                </span>
-            </div>            
-            <p class="my-2">
-                {about}
-            </p>
-        </div>        
-        {/each}
+<JellyContainer class="mt-8">
+    <div class="font-extralight text-7xl text-center mb-8">
+        Selected Yale University Courses
     </div>
-</div>
+    {#each data?.courses as {title, about, professors, listings}}
+    <div class="mt-8">
+        <h2 class="font-light text-4xl my-2">
+            {title}
+        </h2>
+        <div>
+            <h3 class="font-extralight text-2xl my-2">
+                {#each professors as professor, i}
+                    {professor}{i < professors.length - 1 ? " & " : ''}
+                {/each}
+            </h3>
+            <span class="font-semibold text-lg my-2">
+                {#each listings as listing, i}
+                    {listing}{i < listings.length - 1 ? "/" : ''}
+                {/each}
+            </span>
+        </div>            
+        <p class="my-2">
+            {about}
+        </p>
+    </div>        
+    {/each}
+</JellyContainer>
