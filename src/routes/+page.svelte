470ba5393f0ee3607266a7bbd1bb2207afb18816
dsm-72 <100158155+dsm-72@users.noreply.github.com>
2023-02-25 11:27:37 -0500

    fix padding

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,10 +15,10 @@
     </svelte:fragment>
 </Hero>
 
-<div class="py-8"></div> 
+<div class="py-0"></div> 
 
 <JellyContainer>
-    <Hero class="pt-0">What we work on</Hero>
+    <Hero>What we work on</Hero>
     <div class="flex place-content-center pt-8">
         <div class="text-slate-700 leading-relaxed">
             We work at the intersection of computer science, applied math, computational biology, and signal processing to develop representation-learning and deep learning methods that enable exploratory analysis, scientific inference and prediction from big biomedical datasets. We have validated our methods on datasets generated from single-cell RNA sequencing, fMRI, and electronic health records on a wide variety of biological, cellular, and disease systems.  Our techniques generally incorporate mathematical priors from graph spectral theory, manifold learning, signal processing, and topology into machine learning and deep learning frameworks, in order to denoise and model the underlying systems faithfully for predictive insight. Currently our methods are being widely used for data denoising, visualization, generative modeling, dynamics. modeling, comparative analysis and domain transfer. 
