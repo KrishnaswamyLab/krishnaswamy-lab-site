20db9e1aa976f1f976c96e120279c4b1823200af
dsm72 <sumner.magruder@yale.edu>
2023-02-26 18:25:54 -0500

    prose on teaching

--- src/routes/teaching/+page.svelte
+++ src/routes/teaching/+page.svelte
@@ -15,27 +15,30 @@ let backgroundImage = "/images/heroes/teaching_hero.jpg"
 <JellyContainer class="mt-8">
     <Hero class="break-keep">
         Selected Yale<wbr> Uni<wbr>versity Courses
-    </Hero>    
-    {#each data?.courses as {title, about, professors, listings}}
-    <div class="mt-8">
-        <h2 class="font-light text-2xl md:text-4xl my-2">
-            {title}
-        </h2>
-        <div>
-            <h3 class="font-extralight text-xl md:text-2xl my-2">
-                {#each professors as professor, i}
-                    {professor}{i < professors.length - 1 ? " & " : ''}
-                {/each}
-            </h3>
-            <span class="font-semibold text-md md:text-lg my-2">
-                {#each listings as listing, i}
-                    {listing}{i < listings.length - 1 ? "/" : ''}
-                {/each}
-            </span>
-        </div>            
-        <p class="my-2">
-            {about}
-        </p>
-    </div>        
-    {/each}
+    </Hero>
+    <div class="mt-8 flex flex-col place-content-center place-items-center">
+        {#each data?.courses as {title, about, professors, listings}}
+        <div class="mt-8 prose">
+            <h2 class="font-light text-2xl md:text-4xl my-2">
+                {title}
+            </h2>
+            <div>
+                <h3 class="font-extralight text-xl md:text-2xl my-2">
+                    {#each professors as professor, i}
+                        {professor}{i < professors.length - 1 ? " & " : ''}
+                    {/each}
+                </h3>
+                <span class="font-semibold text-md md:text-lg my-2">
+                    {#each listings as listing, i}
+                        {listing}{i < listings.length - 1 ? "/" : ''}
+                    {/each}
+                </span>
+            </div>            
+            <p class="my-2">
+                {about}
+            </p>
+        </div>        
+        {/each}
+    </div>    
+    
 </JellyContainer>
