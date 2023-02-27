fc027bcc2faac6ffad875c0016c05cf17453e0a5
dsm-72 <100158155+dsm-72@users.noreply.github.com>
2023-02-27 08:57:33 -0500

    fix member sorting

--- src/routes/members/+page.ts
+++ src/routes/members/+page.ts
@@ -5,11 +5,9 @@ import members from '$lib/data/members.json'
 export function load({ }) {
     return {
         members: members.map(e=>e as Member).sort((a, b)=>{
-            return a?.image 
-                ? 0
-                : b?.image 
-                    ? 1
-                    : -1                
+            let imgA = a?.image ? 1 : 0
+            let imgB = b?.image ? 1 : 0
+            return imgB - imgA            
         })
     };
   }
