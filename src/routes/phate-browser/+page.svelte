25276065170fd9fd6b164dd6ac82be7fffa96fc1
dsm-72 <100158155+dsm-72@users.noreply.github.com>
2023-03-14 15:05:33 -0400

    phate browser route

---
+++ src/routes/phate-browser/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    /** @type {import('./$types').PageData} */
+    export let data;   
+    import Hero from '$lib/Layout/Hero/Hero.svelte';
+    
+    import JellyContainer from '$lib/Layout/JellyContainer.svelte';
+    
+    import { onMount } from 'svelte'
+    import { goto } from '$app/navigation';
+
+
+    const backgroundImage="/images/lab_hero.jpg"
+    
+    import TwitterSEO from '$lib/SEO/Twitter.svelte'
+    import OpenGraphSEO from '$lib/SEO/OpenGraph.svelte'
+</script>
+<TwitterSEO/>
+<OpenGraphSEO/>
+<Hero></Hero>
