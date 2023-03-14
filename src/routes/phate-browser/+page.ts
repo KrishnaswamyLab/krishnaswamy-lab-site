f4b03b55aecf23b98ddfecc177c269d62af8423b
dsm72 <sumner.magruder@yale.edu>
2023-03-14 19:20:04 -0400

    rough draft phate

---
+++ src/routes/phate-browser/+page.ts
@@ -0,0 +1,20 @@
+/** @type {import('./$types').PageLoad} */
+
+import {LoadGlobsAllAtOnces} from '$lib/utils'
+
+// import points from '$lib/data/eb_2018_phate.json'
+// import counts from '$lib/data/eb_2018_counts.json'
+
+const points_json = import.meta.glob('$lib/data/eb_2018_phate.json', {import: 'default'})
+const counts_json = import.meta.glob('$lib/data/eb_2018_counts.json', {import: 'default'})
+
+export async function load({ }) {
+
+    // const points = (await LoadGlobsAllAtOnces(points_json))[0]
+    // console.log(points_json[0])
+    const points =  Object.values(points_json)[0]()
+    // const counts =  Object.values(counts_json)[0]()
+    // const counts = (await LoadGlobsAllAtOnces(counts_json))[0]
+    const counts = []
+    return {points, counts};
+}
